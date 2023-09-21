d0545df6f86e9e57a18c40820dee6faba4a157f6
Damife Zion <damifeolaleye@gmail.com>
2023-09-21 16:42:48 +0100

    added cors origin

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 //Initialize dependencies
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
