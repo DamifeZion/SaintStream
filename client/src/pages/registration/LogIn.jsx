952bf9a3a3d105285b308a13befa6279ff186639
Damife Zion <damifeolaleye@gmail.com>
2023-10-10 06:58:56 +0100

    fix: Page not overflowing in tablet mode

--- client/src/pages/registration/LogIn.jsx
+++ client/src/pages/registration/LogIn.jsx
@@ -7,6 +7,7 @@ import { useDocumentTitle } from "../../hooks/useDocumentTitle";
 import { useLogin } from "../../hooks/useLogin";
 import ToastWrapper from "../../components/toast/ToastWrapper";
 
+
 const LogIn = () => {
   useDocumentTitle("Login");
 
@@ -20,8 +21,8 @@ const LogIn = () => {
   } = useLogin();
 
   return (
-    <div className="justify-center min-h-screen pb-4 500:bg-[#08070A] 500:flex 500:flex-col 500:items-center 500:overflow-y-scroll">
-      <div className="flex flex-col bg-[#0D0C0F] h-screen max-w-[480px] 500:h-fit 500:rounded-3xl 500:py-6 500:w-5/6 500:border 500:border-[--dark-gray] 500:overflow-y-scroll ">
+    <div className="justify-center min-h-screen pb-4 500:bg-[#08070A] 500:flex 500:flex-col 500:items-center">
+      <div className="flex flex-col bg-[#0D0C0F] max-w-[480px] h-screen 400:py-6 500:h-fit 500:rounded-3xl 500:w-5/6 500:border 500:border-[--dark-gray]">
         <nav className="flex flex-col px-[--px] py-4 500:hidden ">
           <button onClick={goBack} className="text-3xl w-fit">
             <IoIosArrowBack />
--- client/src/pages/registration/SignUp.jsx
+++ client/src/pages/registration/SignUp.jsx
@@ -31,7 +31,7 @@ const SignUp = () => {
 
   return (
     <div className="justify-center min-h-screen pb-4 500:bg-[#08070A] 500:flex 500:flex-col 500:items-center">
-      <div className="flex flex-col bg-[#0D0C0F] overflow-y-scroll h-screen 500:h-fit 500:rounded-3xl 500:py-6 500:w-5/6 500:border 500:border-[--dark-gray] max-w-[480px]">
+      <div className="flex flex-col bg-[#0D0C0F] overflow-y-scroll h-screen 400:py-6  500:h-fit 500:rounded-3xl 500:py-6 500:w-5/6 500:border 500:border-[--dark-gray] max-w-[480px]">
         <nav className="flex flex-col px-[--px] py-4 500:hidden ">
           <button onClick={goBack} className="text-3xl w-fit">
             <IoIosArrowBack />
